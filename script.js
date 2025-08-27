9bebf45423fcb74d3aae022ca7706f76e90e4bfb
SAHIL THAKUR <sahil20120067926thakur@gmail.com>
2025-08-27 20:34:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-const bands = [
-  'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
-  'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans',
-  'Counterparts', 'oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
-];
+const bands =  ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-const list = document.querySelector("#list");
+const list = document.querySelector("#band");
 
 console.log("Original:", bands);
 
